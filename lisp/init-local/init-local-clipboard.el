a05ba05622f1900ab705ec1ea63337ff626cd2c2
zhaoyiping <zhaoyiping0622@gmail.com>
2022-02-16 12:12:55 +0800

    modify clipboard settings

--- lisp/init-local/init-local-clipboard.el
+++ lisp/init-local/init-local-clipboard.el
@@ -4,18 +4,17 @@
 
 ;;; Code:
 ;; osx clipboard
-(when *is-a-mac*
-  (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
+(when (and *is-a-mac* (not window-system))
+       (defun copy-from-osx ()
+         (shell-command-to-string "pbpaste"))
 
-  (defun paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-        (process-send-string proc text)
-        (process-send-eof proc))))
+       (defun paste-to-osx (text &optional push)
+         (let ((process-connection-type nil))
+           (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+             (process-send-string proc text)
+             (process-send-eof proc))))
 
-  (setq interprogram-cut-function 'paste-to-osx)
-  (setq interprogram-paste-function 'copy-from-osx)
-  )
-(provide 'init-local-clipboard)
+       (setq interprogram-cut-function 'paste-to-osx)
+       (setq interprogram-paste-function 'copy-from-osx))
+  (provide 'init-local-clipboard)
 ;;; init-local-clipboard.el ends here
