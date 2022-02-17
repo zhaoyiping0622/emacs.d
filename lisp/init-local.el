53be440e1a539cb60624cd3f7ee8fe26b2eb7a0c
zhaoyiping <zhaoyiping0622@gmail.com>
2022-02-17 12:53:32 +0800

    change tab size

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,12 @@
 ;;; init-local.el --- customize config
+;;; Commentary:
 
 ;; Set up package.el to work with MELPA
 (require 'package)
+
+;;; Code:
+
+(setq-default tab-width 2)
 
 (maybe-require-package 'evil)
 (maybe-require-package 'keyfreq)
@@ -9,7 +14,7 @@
 (add-to-list 'load-path (expand-file-name "lisp/init-local" user-emacs-directory))
 
 (maybe-require-package 'dired-sidebar)
-(require ' dired-sidebar)
+(require 'dired-sidebar)
 
 (require 'init-local-keyfreq)
 (require 'init-local-evil)
